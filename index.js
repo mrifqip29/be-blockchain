4456fd25ef25bb964cecaa1ddc1f6b8a76d9e4c1
Muhammad Rifqi Prabantoro <45098478+mrifqip29@users.noreply.github.com>
2020-12-15 08:00:51 +0700

    add res.header acces control allow origin: *

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ mongoose
 app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use("/", RouteUser);
 app.use("/trx", RouteTrx);
 
